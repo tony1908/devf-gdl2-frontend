71ed1632bb86acf3b179b13f78df309339ebf5b2
Developer <developer@MBP-17-2068.local>
2018-04-23 20:28:31 -0500

    First commit, upload project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './app';
+import {ApolloProvider as Provider} from 'react-apollo';
+import { ApolloClient} from 'apollo-client';
+import { HttpLink} from 'apollo-link-http';
+import { InMemoryCache} from 'apollo-cache-inmemory';
+
+const httpLink = new HttpLink({
+    uri: ""
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+
+});
+
+
+const container = document.getElementById('container');
+
+render(<Provider client = {client}>
+        <App/>
+      </Provider>
+, container);
